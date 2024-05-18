cb8daab09c3ca0debaba0df09f6dac5b0d2b659a
Lucas Matheus <105503258+LmarDark@users.noreply.github.com>
2024-05-18 14:54:49 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,29 @@
+let display = document.getElementById("display");
+
+function addToDisplay(value) {
+    display.value += value;
+}
+
+function clearDisplay() {
+    display.value = "";
+}
+
+function showExtras() {
+    var img = document.getElementById("extraImage");
+    img.style.opacity = 0;
+    img.style.display = "block";
+    var fadeEffect = setInterval(function () {
+        if (!img.style.opacity) {
+            img.style.opacity = 0;
+        }
+        if (img.style.opacity < 1) {
+            img.style.opacity = parseFloat(img.style.opacity) + 0.1;
+        } else {
+            clearInterval(fadeEffect);
+        }
+    }, 300);
+
+    var audio = document.getElementById("audio");
+    audio.currentTime = 10;
+    audio.volume = 0.5;
+    audio.play();
